c6c8d6270614aee11f04d0bbd2760a4e4a2580ba
Sean Lee Brennan <brennansean6@gmail.com>
2022-03-17 12:37:34 -0500

    Functions progress

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,3 +1,9 @@
+/* CS4850 Socket API Project -- Client
+ * Author:      Sean Brennan
+ * PawPrint:    slbvp6
+ * Student #:   14264291
+ * Description: Computer Networks class project using the Socket API in Java
+ */
 package Client;
 
 import java.io.*;
@@ -9,6 +15,7 @@ public class Client {
     private Socket socket;
     private DataInputStream input;
     private DataOutputStream output;
+    private boolean loggedIn = false;
 
     public Client(String address, int port){
         System.out.println("My chat room client. Version One.\n");
@@ -27,101 +34,16 @@ public class Client {
         String command = "";
         try{
             command = input.readLine(); //Method deprecated, consider switching implementation
-            if(parseInput(command) > 0){
-                output.writeUTF(command); //Send command to the server
-            }
-            output.writeUTF("User entered bad input and died.");
+            //if(parseInput(command) > 0){
+                //output.writeUTF(command); //Send command to the server
+            //}
+            parseInput(command);
+            //output.writeUTF("User entered bad input and died.");
         }
         catch(Exception e){
             System.out.println("Error sending in client");
             System.out.println(e);
         }
-
-        /*
-        //src: https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/Scanner.html
-        Scanner userInput = new Scanner(System.in); //src: https://docs.oracle.com/javase/8/docs/api/java/util/Scanner.html
-        String userID = null;
-        String pass = null;
-        String message = null;
-        boolean bool = true;
-        while(bool){
-            try{
-                //Enhanced Switch src: https://www.geeksforgeeks.org/enhancements-for-switch-statement-in-java-13/
-                switch (userInput.next()) {
-                    case "login" -> { //grab & verify UserID & Password
-                        if (userInput.hasNext()) {
-                            userID = userInput.next(); //grab next token which will be the userID
-                            if (userInput.hasNext()) {
-                                pass = userInput.next(); //grab the next token which will be the password
-                                if(userPassRequirements(userID, pass)){ //Check input requirements in login too
-                                    //to save extra time complexity of scanning list of users we know won't be there
-                                    if(login(userID, pass)){ ////successfully retrieved UserID & Pass from user input & logged in
-                                        login(userID, pass);
-                                        bool = false; //break loop
-                                    }
-                                }
-                                else{
-                                    System.out.println("Input invalid, ensure username is between 3 & 32 characters & password is between 4 & 8 characters.");
-                                    userInput.reset();
-                                }
-                                //Prompt server to login client here
-                            }
-                        }
-                    }
-                    case "newuser" -> {
-                        if (userInput.hasNext()) {
-                            userID = userInput.next();
-                            if (userInput.hasNext()) {
-                                pass = userInput.next();
-                                if(userPassRequirements(userID, pass)){ //Double check length requirements
-                                    bool = false; //successfully retrieved UserID & Pass from user input, break loop
-                                    newUser(userID, pass); //Prompt server to create newuser here
-                                }
-                                else{
-                                    System.out.println("Input invalid, ensure username is between 3 & 32 characters & password is between 4 & 8 characters.");
-                                    userInput.reset();
-                                }
-                            }
-                        }
-                    }
-                    case "send" -> {
-                        System.out.println("send command called");
-                        if(userInput.hasNext()){
-                            message = userInput.nextLine();
-                            while(message.equals("")){ //This was causing issues with multi line command inputs
-                                message = userInput.nextLine();
-                            }
-                            //System.out.println("Message:\n" + message); //message retrieved successfully!
-                            if(message.length() >= 1 && message.length() <=256){
-                                bool = false;
-                                //send message to server here
-                            }
-                            else{
-                                System.out.println("Message too long, must be under 256 characters.");
-                                userInput.reset();
-
-                            }
-                        }
-                    }
-                    case "logout" -> {
-                        System.out.println("logout command called");
-                        //If currently logged in then bool = false
-                        //bool = false;
-                    }
-                    default -> {
-                        System.out.println("Not a valid command; 1. login, 2. newuser, 3. send, 4. logout");
-                        //userInput = new Scanner(System.in);
-                        userInput.reset();
-                    }
-                }
-            }
-            catch(Exception e){
-                System.out.println("Exception Thrown: " + e);
-            }
-        }
-
-         */
-//        disconnect();
     }
 
     private boolean userPassRequirements(String userID, String pass){
@@ -130,7 +52,7 @@ public class Client {
     private boolean messageRequirements(String message){
         return (message.length() <= 256 && message.length() >= 1);
     }
-    private int parseInput(String command){ //Returns <= 0 for invalid user inputs
+    private void parseInput(String command){ //Returns <= 0 for invalid user inputs
         //Need to make sure we're passing valid input to the server first
         StringTokenizer tokenizer = new StringTokenizer(command);
         String function = tokenizer.nextToken(); //Get the primary function user is trying to call
@@ -138,28 +60,35 @@ public class Client {
             case "login" ->{
                 String userID = tokenizer.nextToken();
                 String pass = tokenizer.nextToken();
-                return userPassRequirements(userID, pass) ? 1 : -1; //Return positive if meets requirements
+                login(userID, pass);
+//                return userPassRequirements(userID, pass) ? 1 : -1; //Return positive if meets requirements
             }
             case "newuser" ->{
                 String userID = tokenizer.nextToken();
                 String pass = tokenizer.nextToken();
-                return userPassRequirements(userID, pass) ? 2 : -2;
+                newUser(userID, pass);
+//                return userPassRequirements(userID, pass) ? 2 : -2;
             }
             case "send" ->{
                 String message = tokenizer.nextToken();
-                return messageRequirements(message) ? 3 : -3;
+                send(message);
+//                return messageRequirements(message) ? 3 : -3;
             }
             case "logout" ->{
-                return 4;
+                logout();
+//                return logout() ? 4 : -4;
             }
             default ->{
                 System.out.println("Not a valid command; 1. login, 2. newuser, 3. send, 4. logout");
-                return 0;
+//                return 0;
             }
         }
     }
-    private boolean disconnect(){
-        //Disconnect
+    private boolean logout(){//Log out of user client, logout, then close
+        if(!loggedIn){
+            System.out.println("You're not even logged in bro...");
+            return false;
+        }
         try{
             System.out.println("Disconnecting from server...\n");
             input.close();
@@ -172,13 +101,16 @@ public class Client {
             return false;
         }
     }
-    private boolean logout(){
-        //Log out of user client, logout, then close
-        return disconnect();
-
-    }
-    private boolean login(String username, String password){
-        //Send input to server to log user in
+    private boolean login(String userID, String pass){
+        if(loggedIn){
+            System.out.println("Already logged in.");
+            return false;
+        }
+        if(!userPassRequirements(userID, pass)){ //Invalid input, don't bog server down with an unnecessary request
+            System.out.println("Username & password length requirements not correct, cannot login");
+            return false;
+        }
+        //Send input to server to log user in, if server responds w/ true then set loggedIn variable to true
         try{
             output = new DataOutputStream(socket.getOutputStream());
             output.writeUTF(input.readLine());
@@ -190,8 +122,12 @@ public class Client {
     }
     private boolean send(String message){
         //Error checking
+        if(!loggedIn){
+            System.out.println("You need to log in first.");
+            return false;
+        }
         if(message.length() > 256 || message.length() < 1){
-            System.out.println("Sorry, message needs to be between 1 & 256 characters.\n");
+            System.out.println("Sorry, message needs to be between 1 & 256 characters.");
             return false;
         }
 
@@ -207,25 +143,20 @@ public class Client {
             return false;
         }
     }
-    private boolean newUser(String username, String password) { //Client.Client checks correct usage of command then sends to the server
-        //Open userlist
-        //Check if username is already taken, else make user and append to userlist.
-        try {
-            File file = new File("../../users.txt"); //src: https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) { //O(n) for n user entries
-                String user = scanner.nextLine();
-                if (user.equals(username)) { //Almost forgot .equals() method from 3330
-                    System.out.println(user + " already taken, please try again with another username.\n");
-                    return false;
-                }
-            }
-            System.out.println("user " + username + " not found. Creating user!\n");
-
-            return true;
-        } catch (FileNotFoundException fnf) {
-            System.out.println(fnf);
+    private boolean newUser(String userID, String pass) { //Check correct usage of command then send to server
+        if(loggedIn){
+            System.out.println("Can't make new user while logged in.");
             return false;
         }
+        if(userPassRequirements(userID, pass)){ //If input is good then send it off to server
+            try{
+                output = new DataOutputStream(socket.getOutputStream());
+                output.writeUTF("newuser " + userID + " " + pass);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+        return false;
     }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,30 +1,48 @@
+/* CS4850 Socket API Project -- Server
+ * Author:      Sean Brennan
+ * PawPrint:    slbvp6
+ * Student #:   14264291
+ * Description: Computer Networks class project using the Socket API in Java
+ */
 package Server;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.net.ServerSocket; //https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/net/ServerSocket.html
+import java.net.Socket; //https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/net/Socket.html
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List; //https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/List.html
+import java.util.ArrayList; //https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/ArrayList.html
+import java.util.Scanner; //https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/Scanner.html
+import java.util.StringTokenizer; //https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/StringTokenizer.html
 
 public class Server {
     //src:https://docs.oracle.com/javase/8/docs/api/java/nio/channels/SocketChannel.html
     private ServerSocket serverSocket; //Server.Server needs special server socket
     private Socket socket;
     private DataInputStream input;
+    private DataOutputStream output; //To send messages back to the client
+    //Global loggedIn state? Not scalable for V2
+    //private boolean loggedIn = false;
+    private List<String> loggedInList = new ArrayList<String>();
+    private boolean kill = false;
 
     public Server(int port) {
         System.out.println("My chat room server. Version One.\n");
-        //Initialize the server
-        //May not be very scalable to have server load all users & passwords into memory; scan file directly instead?
         try {
             serverSocket = new ServerSocket(port);
-            //Waiting for client to send its output over socket...
-            socket = serverSocket.accept();
-            System.out.println("Client connected!");
-            //BufferedInputStream to avoid EOFException src: https://stackoverflow.com/questions/17972172/eofexception-in-readutf
-            input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-            processInput();
-            //closeConnection();
+            //Waiting for client to connect over socket...
+            while(!kill){
+                socket = serverSocket.accept();
+                System.out.println("Client connected!");
+                //BufferedInputStream to avoid EOFException src: https://stackoverflow.com/questions/17972172/eofexception-in-readutf
+                input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                processInput();
+            }
+            close();
         } catch (IOException io) {
             System.out.println(io);
         }
@@ -57,8 +75,8 @@ public class Server {
     }
 
     private boolean logout() {
-        System.out.println("Logout");
         return true;
+//        return loggedInList.contains(userID) && loggedInList.remove(userID);
     }
 
     private boolean send(String message) {
@@ -66,7 +84,7 @@ public class Server {
         return true;
     }
 
-    public boolean closeConnection() {
+    public boolean close() {
         try {
             System.out.println("Closing server...\n");
             socket.close();
@@ -81,7 +99,7 @@ public class Server {
         String clientInput = "(" + username + ", " + password + ")"; //Form tuple-like format from input like given .txt
         System.out.println("Looking for " + clientInput);
         try{
-            File file = new File("../../users.txt"); //src: https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html
+            File file = new File("../users.txt"); //src: https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html
             Scanner scanner = new Scanner(file);
             while(scanner.hasNextLine()){ //O(n) for n user entries
                 String user = scanner.nextLine();
@@ -89,6 +107,8 @@ public class Server {
                     //Accept login & send confirmation message to client
                     System.out.println("Logging in...\n");
                     //Implement real "login" here
+                    //loggedIn = true;
+                    loggedInList.add(username);
                     return true;
                 }
             }
@@ -101,21 +121,29 @@ public class Server {
             return false;
         }
     }
-    public boolean newUser(String username, String password) {
+    public boolean newUser(String userID, String pass) {
         //Open userlist
         //Check if username is already taken, else make user and append to userlist.
         try {
-            File file = new File("../../users.txt"); //src: https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html
+            File file = new File("../users.txt"); //src: https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) { //O(n) for n user entries
                 String user = scanner.nextLine();
-                if (user.equals(username)) { //Almost forgot .equals() method from 3330
+                if (user.equals(userID)) { //Almost forgot .equals() method from 3330
                     System.out.println(user + " already taken, please try again with another username.\n");
                     return false;
                 }
             }
-            System.out.println("user " + username + " not found. Creating user!\n");
-
+            System.out.println("user " + userID + " not found. Creating user!\n");
+            String entry = "\n(" + userID + ", " + pass + ")";
+            try{
+                //src: https://stackoverflow.com/questions/1625234/how-to-append-text-to-an-existing-file-in-java
+                Files.write(Paths.get("../users.txt"), entry.getBytes(), StandardOpenOption.APPEND);
+                //Send message to client that user was created
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
             return true;
         } catch (FileNotFoundException fnf) {
             System.out.println(fnf);
